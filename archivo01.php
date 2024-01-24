01e002acb7f396507a37d9a2571d52ad1e496a16
nando12354 <nandito12354@gmail.com>
2024-01-24 14:20:40 -0500

    Archivo01 login

--- archivo01.php
+++ archivo01.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Archivo01 v2</title>
+    <title>Archivo01 v login</title>
 </head>
 <body>
     
